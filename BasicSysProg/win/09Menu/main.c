7d160e92c43d15fdc5b16005f6b48d0e5d51bf05
AceBear <vast_m_bear@yahoo.com>
2016-06-23 17:13:14 +0800

    09-menu-init

---
+++ BasicSysProg/win/09Menu/main.c
@@ -0,0 +1,89 @@
+#include "stdafx.h"
+
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrev, PWSTR pCmdLine, int nCmdShow)
+{
+    wchar_t wszTitle[] = L"Menu";
+
+    WNDCLASS wcls;
+    ZeroMemory(&wcls, sizeof(WNDCLASS));
+    wcls.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+    wcls.lpfnWndProc = WindowProc;
+    wcls.hInstance = hInstance;
+    wcls.hbrBackground = GetStockObject(LTGRAY_BRUSH);
+    wcls.hIcon = LoadIcon(NULL, IDI_SHIELD);
+    wcls.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcls.lpszClassName = wszTitle;
+
+    if (!RegisterClass(&wcls)) {
+        MessageBox(NULL, L"Register class failed", L"Error", MB_OK);
+        return GetLastError();
+    }
+
+    int cxScreen = GetSystemMetrics(SM_CXSCREEN);
+    int cyScreen = GetSystemMetrics(SM_CYSCREEN);
+    int width = cxScreen > 800 ? 800 : cxScreen;
+    int height = cyScreen > 600 ? 600 : cyScreen;
+
+    HWND hWndMain = CreateWindow(wszTitle, L"²Ëµ¥", WS_OVERLAPPEDWINDOW, (cxScreen - width) / 2, (cyScreen - height) / 2, width, height, NULL, NULL, hInstance, NULL);
+    if (!hWndMain) {
+        MessageBox(NULL, L"Create window failed", L"Error", MB_OK);
+        return GetLastError();
+    }
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+struct APP
+{
+    HWND hStaticUser;
+    HWND hEditUser;
+    HWND hStaticPwd;
+    HWND hEditPwd;
+    HWND hBtnLogin;
+    HFONT hFontText;
+} g_app;
+
+BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
+{
+    ShowWindow(hwnd, SW_SHOW);
+    return TRUE;
+}
+
+void OnDestroy(HWND hwnd)
+{
+    PostQuitMessage(0);
+}
+
+void OnPaint(HWND hwnd)
+{
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hwnd, &ps);
+    EndPaint(hwnd, &ps);
+}
+
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+    FORWARD_WM_COMMAND(hwnd, id, hwndCtl, codeNotify, DefWindowProc);
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg) {
+        HANDLE_MSG(hWnd, WM_CREATE, OnCreate);
+        HANDLE_MSG(hWnd, WM_DESTROY, OnDestroy);
+        HANDLE_MSG(hWnd, WM_PAINT, OnPaint);
+        HANDLE_MSG(hWnd, WM_COMMAND, OnCommand);
+    default:
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+
+    return 0L;
+}
