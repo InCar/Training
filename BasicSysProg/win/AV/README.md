70bd76efcd6db52ea1f4890c52d1c18ed37c9090
AceBear <vast_m_bear@yahoo.com>
2016-11-18 13:42:37 +0800

    audio init

---
+++ BasicSysProg/win/AV/README.md
@@ -0,0 +1,6 @@
+﻿# 音频和视频
+
+## 音频
+![Windows Audio](https://i-msdn.sec.s-msft.com/dynimg/IC45726.jpg)
+
+## 视频
---
+++ BasicSysProg/win/AV/main.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrev, PWSTR pCmdLine, int nCmdShow)
+{
+    return 0;
+}
